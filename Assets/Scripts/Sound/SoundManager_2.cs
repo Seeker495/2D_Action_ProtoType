eb48cc88ea225817d73bcdb681ae054b9575ea6b
hedgehogT <takeuchi.yuu2023@gmail.com>
2022-10-31 23:02:50 +0900

    Addbgm

---
+++ Assets/Scripts/Sound/SoundManager_2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager_2 : MonoBehaviour
+{
+    [System.Serializable]
+    public struct AudioInfo 
+    {
+        public string name;
+        public AudioClip clip;
+    }
+    public List<AudioInfo> m_bgmList = new List<AudioInfo>();
+    public List<AudioInfo> m_seList = new List<AudioInfo>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
